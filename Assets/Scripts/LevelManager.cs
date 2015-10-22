6ddf9bf3f271b62f318085db69d5ace4fc062b2f
ProfessorZ <tom.geldermans@gmail.com>
2015-10-22 22:17:25 +0100

    Made scenes and created 'playflow' through the scenes
    Created Start, Win, Level_01(actual game) scenes and controlled them via
    LevelManager script

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	//Load new level, passes along the name of the level to be loaded
+	public void loadNewLevel(string name){
+		Application.LoadLevel(name);
+		//DontDestroyOnLoad(NumberSettings.settings);		// Makes sure Settings are saved between the levels.
+	}
+
+	public void quitRequest(){
+		Application.Quit ();							// Quits the game
+	}
+	
+}
