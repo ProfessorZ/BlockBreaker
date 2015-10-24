1d9dcc11697cd6609c798b2ebf4d35bb04ecf62a
ProfessorZ <tom.geldermans@gmail.com>
2015-10-24 21:24:22 +0100

    added method loadNextLevel

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,9 @@ public class LevelManager : MonoBehaviour {
 	//Load new level, passes along the name of the level to be loaded
 	public void loadNewLevel(string name){
 		Application.LoadLevel(name);
-		//DontDestroyOnLoad(NumberSettings.settings);		// Makes sure Settings are saved between the levels.
+	}
+	public void loadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel +1);
 	}
 
 	public void quitRequest(){
