7d0799be85a98210f4d04c3c60346765ae156bb7
ProfessorZ <tom.geldermans@gmail.com>
2015-10-24 16:11:49 +0100

    First steps in controlling the paddle
    Goes left and right based on mouse input. Paddle can not go out of
    screen.

---
+++ Assets/PaddleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	float mousePosInBlocks = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
+		this.transform.position = paddlePos;
+	}
+}
