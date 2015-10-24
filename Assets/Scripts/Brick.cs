630ad2a56677ad1c92a67a361292c9fffa969771
ProfessorZ <tom.geldermans@gmail.com>
2015-10-24 17:46:25 +0100

    created Script to control the bricks
    Bricks have a maximum times they have to be hit before destruction.
    Count amount of times hit and destroy brick. (currently 'destroying' a
    brick only logs to console)

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	public int maxHits;
+	private int timesHit;
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		timesHit++;
+		if (timesHit >= maxHits) {
+			Destroy(this);
+			Debug.Log("Brick Kaput!");
+		}
+	}
+}
