d316c473e2948578a06fa12934e85184bc67b565
ProfessorZ <tom.geldermans@gmail.com>
2015-10-24 16:57:25 +0100

    made ball go faster

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -17,7 +17,7 @@ public class Ball : MonoBehaviour {
 		
 			if (Input.GetMouseButtonDown(0)) {
 				hasStarted = true;
-				this.rigidbody2D.velocity = new Vector2 (2f,10f);
+				this.rigidbody2D.velocity = new Vector2 (2f,12f);
 			}
 		}
 	}
