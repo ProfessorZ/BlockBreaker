29377e33d0a497e4d799464c22038843aca898a4
ProfessorZ <tom.geldermans@gmail.com>
2015-10-25 16:49:57 +0000

    GUI effects and move to nextLevel on screen clear
    Added tags to breakable bricks
    Let game advance to next level on screen clear (ie. all breakables are
    destroyed)
    Let bricks change sprite on hit to signify breaking.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class Brick : MonoBehaviour {
+
+	public static int breakableCount = 0;
+
+	public Sprite[] hitSprites;
 	public int maxHits;
 	private int timesHit;
+	private bool isBreakable;
 
 	private LevelManager levelManager;
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			breakableCount++; 
+		}
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	void Awake(){
+		breakableCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,16 +30,35 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collider){
-		timesHit++;
-		/* if (timesHit >= maxHits) {
-			Destroy(gameObject);
-			Debug.Log("Brick Kaput!");
-		}*/
-		SimulateWin();
+		if (isBreakable){
+		HandleHits();
+		}
+
+		Debug.Log (breakableCount.ToString());
 	}
 
+	void HandleHits (){
+		timesHit++;
+		if (timesHit >= hitSprites.Length+1) {
+			breakableCount--;
+			levelManager.BrickDestroyed();
+			Destroy(gameObject);
+			Debug.Log("Brick Kaput!");
+		}
+		else {
+			LoadSprites();
+		}
+	}
 	// TODO Remove this method once we can actually win!
 	void SimulateWin(){
 		levelManager.loadNextLevel();
 	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit-1;
+		if(hitSprites[spriteIndex]){
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
 }
+
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,5 +14,10 @@ public class LevelManager : MonoBehaviour {
 	public void quitRequest(){
 		Application.Quit ();							// Quits the game
 	}
-	
+
+	public void BrickDestroyed(){
+		if(Brick.breakableCount <= 0){
+			loadNextLevel();
+		}
+	}
 }
