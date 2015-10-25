eb5172620bfcf6ab114e598f43710fee0bfc2de7
ProfessorZ <tom.geldermans@gmail.com>
2015-10-25 22:35:43 +0000

    get bug out of the way
    bug that causes the game to not progress after the game has been lost
    once and the game gets started again (breakableBrick count did not get
    reset on LOSE)

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -5,8 +5,8 @@ public class LoseCollider : MonoBehaviour {
 	private LevelManager levelManager;
 
 	void OnTriggerEnter2D(Collider2D trigger){
+		Brick.breakableCount = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
-		Debug.Log("Trigger");
 		levelManager.loadNewLevel("Lose");
 	}
 	void OnCollisionEnter2D(Collision2D collision){
