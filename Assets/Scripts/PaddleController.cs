51197bcf8c1b90904a79f735d13257ec7ed648be
ProfessorZ <tom.geldermans@gmail.com>
2015-10-25 22:34:57 +0000

    more of autoplay

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -2,15 +2,33 @@
 using System.Collections;
 
 public class PaddleController : MonoBehaviour {
-
+	public bool autoplay = false;
+	private Ball ball;
 	float mousePosInBlocks = 0;
-	// Use this for initialization
-	void Start () {
-	
+
+	void Start(){
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
-	
 	// Update is called once per frame
 	void Update () {
+		if (!autoplay) {
+			MoveWithMouse();
+		}
+		else
+		{
+			Autoplay();
+		}
+
+	}
+
+	void Autoplay(){
+		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
+		Vector3 ballPos = ball.transform.position;
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		paddlePos.x = Mathf.Clamp(ballPos.x,0.617f,15.37f);
+		this.transform.position = paddlePos;
+	}
+	void MoveWithMouse(){
 		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
 		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 		paddlePos.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
